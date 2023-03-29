5415a57534e37b8bf0abbc7101c1d76c7809be8b
MAUREEN-OUGODEV <ougomaureenakinyi@gmail.com>
2023-03-29 19:14:21 +0300

    pushing add

---
+++ functions/math.py
@@ -0,0 +1,20 @@
+def add(a,b):
+    answer=a+b
+    return answer
+
+def subtract(c,d):
+    answer = c-d
+    return answer
+
+
+def multiply(d,e):
+    answer= d*e
+    return answer
+
+def division(f,g):
+    answer = f/g
+    return answer
+
+def reminder (k,l):
+    answer = k%l
+    return answer
